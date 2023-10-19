46a7ee2cc811f1b202af1d48665662a1240497f5
pinvokeee <nanasisine+1@gmail.com>
2023-10-20 08:34:51 +0900

    てｓｔ

--- src/fuatures/checksheet/SheetEditor.tsx
+++ src/fuatures/checksheet/SheetEditor.tsx
@@ -1,4 +1,4 @@
-import { Checkbox, FormControlLabel, MenuItem, Select, TextField, styled } from "@mui/material";
+import { Button, Card, Checkbox, FormControlLabel, Grid, MenuItem, Select, Stack, TextField, styled } from "@mui/material";
 import { Sheet, SheetItem } from "../../types/types";
 
 const Container = styled("div")(({ theme }) => ({
@@ -8,27 +8,65 @@ const Container = styled("div")(({ theme }) => ({
 
 }));
 
+const Flex = styled("div")(({ theme }) => ({
+
+    display: "flex",
+    flexDirection: "column",
+    gap: "8px",
+}));
+
+const Item = styled("div")(({ theme }) => ({
+    display: "flex",
+    flexDirection: "row",
+}));
+
+const Label = styled("div")(({ theme }) => ({
+    minWidth: "100px",
+    padding: "4px",
+}));
+
 const Selectors = (props: { items: string[] }) => {
 
 
     return <>
+        <Flex>
+            { props.items.map((item, index) => <Item>
+                <Label>選択肢{index+1}</Label>
+                <TextField key={item} value={item} />
+                </Item>) }
+        </Flex>
     </>
 }
 
-const Item = (props: { target: SheetItem }) => {
+const SheetItemElement = (props: { target: SheetItem }) => {
 
     const item = props.target;
 
     return <>
-        <div>項目</div>
-        <TextField value={item.name} />
-        <div>タイプ</div>
-        <Select value={item.type}>
-            <MenuItem value="text">テキスト</MenuItem>
-            <MenuItem value="checkbox">チェックボックス</MenuItem>
-            <MenuItem value="radio">ラジオボタン</MenuItem>
-        </Select>
-        <FormControlLabel label="必須項目" control={<Checkbox value={item.isRequired}/>}/>
+        <Card sx={{padding: "16px"}}>
+            <Flex>
+                <Item> 
+                    <Label>項目</Label>
+                    <TextField value={item.name} />
+                </Item>
+
+                <Item> 
+                    <Label>タイプ</Label>
+                    <Select value={item.type}>
+                        <MenuItem value="text">テキスト</MenuItem>
+                        <MenuItem value="checkbox">チェックボックス</MenuItem>
+                        <MenuItem value="radio">ラジオボタン</MenuItem>
+                    </Select>
+                </Item>
+
+                { (item.type == "radio" || item.type == "checkbox") && <Selectors items={item.selector} /> }
+
+                <Item> 
+                    <FormControlLabel label="必須項目" control={<Checkbox checked={item.isRequired}/>}/>
+                </Item>
+
+            </Flex>
+        </Card>
     </>
 }
 
@@ -38,7 +76,10 @@ export const SheetEditor = (props: { currentSheet: Sheet }) => {
 
     return <>
         <Container>
-            { items.map(item => <Item key={item.key} target={item} />) }
+            <Stack gap={2}>
+                { items.map(item => <SheetItemElement key={item.key} target={item} />) }
+                <Button>追加</Button>
+            </Stack>
         </Container>
     </>
 
